c64c2e6e007d79f9f7aefe8b95f9c07b13216bec
Pablo <pablo_mayta_17@hotmail.com>
2023-02-11 21:47:55 -0300

    Subo el CV

---
+++ functions.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Además de la fotografía mis otras pasiones son el dibujo y el teatro que me ayudan a encontrar la inspiración para mis fotografías.";
+  }
